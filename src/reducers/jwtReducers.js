4f2d5795cedc9cb015ea35b3243e6bc566025dd2
Jonathan <jvoxland@gmail.com>
2018-07-13 11:24:39 -0700

    verifying the token now sends the user name

--- src/reducers/jwtReducers.js
+++ src/reducers/jwtReducers.js
@@ -11,14 +11,20 @@ const initialToken = {
     user: {
         id: null,
         email: null,
+        name: null,
     },
 };
 export const verifyJWT = (state = initialToken, action = {}) => {
+    console.log(action.payload)
     switch (action.type) {
         case VERIFY_JSON_TOKEN_PENDING:
             return Object.assign({}, state, { isPending: true });
         case VERIFY_JSON_TOKEN_SUCCESS:
-            return Object.assign({}, state, { token: action.payload.token, isPending: false, user: { id: action.payload.user.id, email: action.payload.user.email } });
+            return Object.assign({}, state, {
+                token: action.payload.token, isPending: false,
+                user: { id: action.payload.user.id, email: action.payload.user.email, name: action.payload.name }
+            }
+            );
         case VERIFY_JSON_TOKEN_FAILED:
             return Object.assign({}, state, { error: action.payload, isPending: false });
         default:
