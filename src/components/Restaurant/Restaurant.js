9982e566d85f8929895fe70e2f82aadf52a69839
Jonathan <jvoxland@gmail.com>
2018-07-17 20:15:06 -0700

    can now view edit form

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import Navigation from '../Presentational/Navigation'
+import Navigation from '../Presentational/Navigation';
+import EditForm from '../User/EditForm';
+import Button from '../Presentational/Button';
 
 
+const Form = (props) => {
+    console.log(props);
+    if (props.showForm) {
+        return (
+            <EditForm restauranId={props.restaurantId} />
+
+        )
+    } else {
+        return null;
+    }
+}
 
 class Restaurant extends Component {
     constructor(props) {
         super(props);
         const restaurantId = this.props.match.params.id;
         props.fetchRestaurantById(restaurantId);
+        this.state = {
+            showForm: false,
+        }
     }
-    
+    showHideForm = () => {
+        this.setState((prevState, props) => {
+            return {
+                showForm: true
+            }
+        })
+    }
+
     render() {
         if (this.props.restaurantLoaded) {
             return (
@@ -18,6 +41,8 @@ class Restaurant extends Component {
                     <h1>{this.props.restaurantById.title}</h1>
                     <p>{this.props.restaurantById.description}</p>
                     <p>{this.props.restaurantById.drinks}</p>
+                    <Form restauranId={this.restaurantId} showForm={this.state.showForm} />
+                    <Button buttonDesc={'Edit Restaurant'} clickAction={this.showHideForm} funcArgs={null} />
                 </div>
             )
         } else {
---
+++ src/components/User/EditForm.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+import Notification from '../Presentational/Notification';
+
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 200,
+    },
+    menu: {
+        width: 200,
+    },
+    button: {
+        margin: theme.spacing.unit,
+    },
+    input: {
+        display: 'none',
+    },
+});
+
+class EditForm extends React.Component {
+    constructor(props) {
+        super();
+    }
+    state = {
+        title: '',
+        description: '',
+        drinks: '',
+    };
+
+    handleSubmit = (event, data) => {
+        axios({
+            method: 'patch',
+            url: 'http://localhost:8080/updateRestaurant/:id',
+            data: {
+                title: this.state.title,
+                description: this.state.description,
+                drinks: this.state.drinks
+            }
+        }).then((response) => {
+            // const user_created = true;
+            // this.props.accountJustCreated(user_created);
+        }).catch((err) => {
+            const errorMessage = err.response.data.message;
+            this.props.renderResponse(errorMessage)
+        });
+        event.preventDefault();
+    };
+
+    handleChange = (email, password_digest, name) => event => {
+        this.setState({
+            [name]: event.target.value,
+            [email]: event.target.value,
+            [password_digest]: event.target.value,
+        });
+    };
+
+    render() {
+        const { classes } = this.props;
+        if (this.props.loggedIn) {
+            return (
+                <Redirect to="/home" />
+            )
+        } else {
+            return (
+                <div>
+                    <form id='signup-form' noValidate autoComplete='off' onSubmit={this.handleSubmit}>
+                        <TextField
+                            id="name"
+                            className={classes.textField}
+                            placeholder="Name"
+                            label="Name"
+                            value={this.state.name}
+                            onChange={this.handleChange('name')}
+                            margin="normal"
+                        />
+                        <TextField
+                            id="email"
+                            className={classes.textField}
+                            label="Email"
+                            value={this.state.email}
+                            onChange={this.handleChange('email')}
+                            margin="normal"
+                        />
+                        <TextField
+                            id="password-input"
+                            className={classes.textField}
+                            placeholder="Password"
+                            label="Password"
+                            type="password"
+                            onChange={this.handleChange('password_digest')}
+                            margin="normal"
+                        />
+                        <Button variant="contained" type='submit' color="primary">
+                            Create Account
+            </Button>
+                    </form>
+                    <Notification
+                        responseMessage={this.state.responseMessage}
+                        showNotification={this.state.showNotification}
+                        clearNotification={this.clearNotification}
+                    />
+                </div>
+            );
+        }
+    }
+}
+
+export default withStyles(styles)(EditForm);
